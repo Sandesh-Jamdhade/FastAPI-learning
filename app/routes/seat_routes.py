8134f2c5bae5e76d55401608c275530bbd16a527
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-22 00:27:40 +0530

    Added create seat routes

---
+++ app/routes/seat_routes.py
@@ -0,0 +1,32 @@
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from ..database import SessionLocal
+from ..models import Seat
+
+router = APIRouter(prefix="/seats", tags=["Seats"])
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+@router.post("/")
+def create_seat(seat_number: str, db: Session = Depends(get_db)):
+
+    existing_seat = db.query(Seat).filter(Seat.seat_number == seat_number).first()
+
+    if existing_seat:
+        raise HTTPException(status_code=400, detail="Seat already exists")
+
+    seat = Seat(seat_number=seat_number)
+    db.add(seat)
+    db.commit()
+    db.refresh(seat)
+
+    return seat
+
+@router.get("/")
+def list_seats(db: Session = Depends(get_db)):
+    return db.query(Seat).all()
