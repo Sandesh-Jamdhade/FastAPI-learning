8b43eb85cd3661cc10bdfcc6123a535c4aabe02b
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-22 01:48:05 +0530

    Added booking endpoint

--- app/routes/booking_routes.py
+++ app/routes/booking_routes.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from ..database import SessionLocal
 from ..models import Seat, Booking
 from ..schemas import BookingCreate
+from ..auth_dependency import get_current_user
 
 router = APIRouter(prefix="/booking", tags=["Booking"])
 
@@ -15,20 +16,30 @@ def get_db():
 
 
 @router.post("/")
-def book_seat(data: BookingCreate, db: Session = Depends(get_db)):
+def book_seat(
+    data: BookingCreate,
+    db: Session = Depends(get_db),
+    user = Depends(get_current_user)  
+):
 
     seat = db.query(Seat).filter(Seat.id == data.seat_id).first()
 
     if not seat:
-        raise HTTPException(404, "Seat not found")
+        raise HTTPException(status_code=404, detail="Seat not found")
 
     if seat.is_booked:
-        raise HTTPException(400, "Seat already booked")
+        raise HTTPException(status_code=400, detail="Seat already booked")
 
     seat.is_booked = True
 
-    booking = Booking(user_name=data.user_name, seat_id=data.seat_id)
+    booking = Booking(
+        user_name=user,   
+        seat_id=data.seat_id
+    )
+
     db.add(booking)
     db.commit()
 
-    return {"message": "Seat booked successfully"}
+    return {
+        "message": f"Seat booked successfully by {user}"
+    }
