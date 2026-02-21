e9cbc4c123f666a93a83a5580e6a5c41197712a8
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-21 20:55:28 +0530

    Created Database model for the movie bookings

---
+++ app/models.py
@@ -0,0 +1,37 @@
+from sqlalchemy import Column, Integer, String, ForeignKey, Boolean, UniqueConstraint
+from .database import Base
+
+class Movie(Base):
+    __tablename__ = "movies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    genre = Column(String)
+
+    __table_args__ = (
+        UniqueConstraint("title", name="unique_movie_title"),
+    )
+
+class Show(Base):
+    __tablename__ = "shows"
+
+    id = Column(Integer, primary_key=True, index=True)
+    movie_id = Column(Integer, ForeignKey("movies.id"))
+    show_time = Column(String)
+
+
+class Seat(Base):
+    __tablename__ = "seats"
+
+    id = Column(Integer, primary_key=True, index=True)
+    show_id = Column(Integer, ForeignKey("shows.id"))
+    seat_number = Column(Integer)
+    is_booked = Column(Boolean, default=False)
+
+
+class Booking(Base):
+    __tablename__ = "bookings"
+
+    id = Column(Integer, primary_key=True, index=True)
+    user_name = Column(String)
+    seat_id = Column(Integer, ForeignKey("seats.id"))
