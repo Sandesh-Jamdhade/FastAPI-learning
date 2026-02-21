e510b61330aa8df218023330961d602df952c246
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-22 01:58:21 +0530

    Included User routes into main file

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI
-from app.database import Base, engine 
-from app.routes import movie_routes, show_routes, booking_routes, seat_routes
+from .database import Base, engine
+from .routes import movie_routes, show_routes, booking_routes, seat_routes, user_routes
+from .scheduler import start_scheduler
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Movie Booking API")
 
+start_scheduler()
 
+app.include_router(user_routes.router)
 app.include_router(seat_routes.router)
 app.include_router(movie_routes.router)
 app.include_router(show_routes.router)
 app.include_router(booking_routes.router)
-
-
