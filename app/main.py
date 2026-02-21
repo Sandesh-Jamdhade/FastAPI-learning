d43bc12d88c5cbe43739a52e48e1c41778774b9c
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-21 21:31:33 +0530

    Included movies,shows and booking routers

--- app/main.py
+++ app/main.py
@@ -1,59 +1,14 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from typing import Optional
+from app.database import Base, engine 
 
-app = FastAPI()
-students={
-    1:{
-        "name":"Sandesh",
-        "age":21,
-        "Degree":"MCA"
-    }
-}
+from app.routes import movie_routes, show_routes, booking_routes
 
-#Fetching the Data of the user
+Base.metadata.create_all(bind=engine)
 
-@app.get("/get-student/{student_id}")
-def get_student(student_id:int):
-    return students[student_id]
+app = FastAPI(title="Movie Booking API")
 
-#Create New Student
-class Student(BaseModel):
-    name:str
-    age:int
-    Degree:str
+app.include_router(movie_routes.router)
+app.include_router(show_routes.router)
+app.include_router(booking_routes.router)
 
-@app.post("/create-student/{student_id}")
-def create_student(student_id: int, student: Student):
-    if student_id in students:
-        return {"Error": "Already Exists"}
-
-    students[student_id] = student
-    return {"message": "Student created"}
-
-#Update Existing Student
-class UpdateStudent(BaseModel):
-    name:Optional[str]=None
-    age:Optional[int]=None
-    Degree:Optional[str]=None
-
-@app.put("/update-student/{student_id}")
-def update_student(student_id:int,student:UpdateStudent):
-    if student_id not in students:
-        return{"Data":"Not Exist"}
-    if student.name!=None:
-        students[student_id].name=student.name
-    if student.age!=None:
-        students[student_id].age=student.age
-    if student.Degree!=None:
-        students[student_id].Degree=student.Degree
-    return students[student_id]
-
-#Deleting Student
-@app.delete("/delete-student/{student_id}")
-def delete_student(student_id:int):
-    if student_id not in students:
-        return{"Id":"Not Exist"}
-    del students[student_id]
-    return{"Message":"Student deleted"}
 
