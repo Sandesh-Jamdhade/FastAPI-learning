ee0884ad04f0f5ad7873a898f85424c6a3626153
Sandesh-Jamdhade <sandeshjamdhade26@gmail.com>
2026-02-22 16:23:40 +0530

    add token validation and blacklist support

--- app/auth_dependency.py
+++ app/auth_dependency.py
@@ -1,6 +1,7 @@
 from fastapi import Depends, HTTPException
 from fastapi.security import OAuth2PasswordBearer
 from jose import jwt, JWTError
+from .redis_client import r
 
 SECRET_KEY = "secret"
 ALGORITHM = "HS256"
@@ -8,6 +9,9 @@ ALGORITHM = "HS256"
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 def get_current_user(token: str = Depends(oauth2_scheme)):
+
+    if r.exists(f"blacklist:{token}"):
+        raise HTTPException(status_code=401, detail="Token revoked")
 
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
